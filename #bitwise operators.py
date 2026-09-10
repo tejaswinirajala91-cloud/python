a7a049751c021e8668db82f1b8bb3872e089ac83
tejaswinirajala91-cloud <tejaswinirajala91@gmail.com>
2026-09-10 15:19:28 +0530

    Add files via upload

--- #bitwise operators.py
+++ #bitwise operators.py
@@ -6,3 +6,31 @@ print(a|b)
 print(a^b)
 print(a << b)
 print(a >> b)
+
+#electric city bill calculator
+units = int(input("Enter electricity units:"))
+
+rate = 6
+
+bill = units*rate
+print("Electicity Bill:", bill)
+
+#travel expense calculator
+travel = float(input("Travel expense:"))
+food = float(input("Food expense:"))
+hotel = float(input("Hotel expense:"))
+
+total = travel + food + hotel
+
+print("Total Expense:", total)
+
+#list in python
+#List is an ordered and changesble collection that can store
+marks = [80,90,70,85]
+print(marks)
+
+#accessing elements in a list
+marks = [80,90,75,85]
+print(marks[0])
+print(marks[1])
+print(marks[3])
---
+++ datatypes.py
@@ -0,0 +1,124 @@
+#list in python
+#list is an ordered and changeable collection that can store and list is a mutable
+marks = [80,90,75,85]
+print(marks)
+
+#accessing elements in a list
+marks = [80,90,75,85]
+print(marks[0])
+print(marks[1])
+print(marks[3])
+
+#change elements in a list
+marks = [80,90,75]
+marks[1] = 95
+print(marks)
+
+#add elements to a list
+marks = [80,90,75]
+marks.append(85)
+print(marks)
+
+#remove elements from a list
+marks = [80,90,75]
+marks.remove(90)
+print(marks)
+
+#insert method
+numbers = [10,20,30]
+numbers.insert(1,15)
+print(numbers)
+
+#extend method
+a = [1,2,3]
+b = [4,5,6]
+a.extend(b)
+print(a)
+
+#clear method
+numbers = [10,20,30]
+numbers.clear()
+print(numbers)
+
+#index method
+numbers = [10,20,30,40]
+print(numbers.index(30))
+
+#count method
+numbers = [10,20,20,30,20]
+print(numbers.count(20))
+
+#sort method
+numbers = [40,10,30,20]
+numbers.sort()
+print(numbers)
+numbers.sort(reverse=True)
+print(numbers)
+
+#reverse method
+numbers = [10,20,30,40]
+numbers.reverse()
+print(numbers)
+
+#copy method
+a = [1,2,3]
+b = a.copy()
+print(b)
+
+numbers = [10,20,30,40,50]
+print(numbers[1:4])
+print(numbers[:3])
+print(numbers[2:])
+print(numbers[::-1])
+
+
+#tuples in python
+#Tuple is a collection of multiple values that is ordered and cannot be changed after creation and it is not mutable
+student = ("Bhargavi",98,"Python")
+print(student[0])
+
+#access values in a tuple
+student = ("Bhargavi",21,85.5)
+print(student[0])
+print(student[1])
+print(student[2])
+
+#immutable nature of tuples
+
+
+#tuples are immutable,meaning they cannot be changed after 
+numbers = (10,20,20,30,20)
+print(numbers.count(20))
+
+numbers = (10,20,30,40)
+print(numbers.index(30))
+
+numbers = (10,20,30,40)
+print(len(numbers))
+print(max(numbers))
+print(min(numbers))
+print(sum(numbers))
+
+#sets in a python
+#set is a collection of unique values that is unorderd and mutable
+numbers = {10,20,30,20,10}
+print(numbers)
+
+#why use set?
+
+#suppose students have selected subjects
+subjects = {"Python","Java","Python","SQL","Java"}
+print(subjects)
+
+#add values to a set
+subjects = {"Python","Java"} 
+subjects.add("SQL")
+print(subjects)
+
+#remove values from a set
+subjects.remove("Java")
+print(subjects)
+
+#Sets do not allow duplicate values
+numbers = {1,2,2,3,3,4}
+print(numbers)
